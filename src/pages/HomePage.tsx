34119259b84d86b3d698d6084d1a628e0e8f2b89
= <=>
2024-11-29 17:30:05 +0400

    fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { HeroSection } from '../components/HeroSection';
 import { CarListing } from '../components/CarListing';
 import { HowItWorks } from '../components/HowItWorks';
--- src/pages/WhyChooseUsPage.tsx
+++ src/pages/WhyChooseUsPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { WhyChooseUs } from '../components/WhyChooseUs';
 
 interface WhyChooseUsPageProps {
