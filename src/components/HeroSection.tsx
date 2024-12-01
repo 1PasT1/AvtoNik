085cc2e91948c050df257aa327102d2c49fffd28
= <=>
2024-12-02 02:58:46 +0400

    fix

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -23,7 +23,7 @@ export function HeroSection({ language }: HeroSectionProps) {
             </div>
             <div className="relative">
                 <img
-                  src="/assets/SUV.png"
+                  src="./assets/SUV.png"
                   alt={
                     language === "English"
                       ? "Orange Jeep Wrangler"
