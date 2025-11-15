9cf9f6eb75cde1d7537556425bb54947740b889a
Stafilo <nikazedginidze44@gmail.com>
2025-11-15 20:42:53 +0400

    Add pricing tiers to Car interface

--- src/types/car.ts
+++ src/types/car.ts
@@ -2,6 +2,10 @@ export interface Car {
   id: string;
   name: string;
   price: number;
+  price_1_3: number;
+  price_4_7: number;
+  price_8_30: number;
+  price_30_plus: number;
   image: string;
   category: string;
   transmission: string;
